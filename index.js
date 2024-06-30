332a33dace3b20055add964a3412f66b395eac40
MiguelAssis0 <163607001+MiguelAssis0@users.noreply.github.com>
2024-06-30 20:12:46 +0000

    porta alterada

--- index.js
+++ index.js
@@ -187,7 +187,8 @@ app.get('/admin/login', (req, res) => {
 })
 
 
-
-
-app.listen(3000, () => { })
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
