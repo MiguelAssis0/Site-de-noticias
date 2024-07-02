ae357e0e8d262603359cd1e3c4a326ff5e10df17
Miguel Kawe dos Anjos Assis <miganjos88@gmail.com>
2024-07-02 14:17:47 -0300

    vercel

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ app.get('/admin/login', (req, res) => {
 })
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
